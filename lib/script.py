367cb91d78c40e72312ec63f0743de50c7c31b90
Addy Rodriguez <rdz.33.a@gmail.com>
2022-02-17 22:58:58 -0600

    Working on if else statements

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,42 @@
 from capitals import states
 
 print(states)
+
+# states = [
+# {
+#     "name": "Alabama",
+#     "capital": "Montgomery"
+# }, {
+#     "name": "Alaska",
+#     "capital": "Juneau"
+# }, {
+#     "name": "Arizona",
+#     "capital": "Phoenix"
+# }, {
+#     "name": "Arkansas",
+#     "capital": "Little Rock"
+
+# def random_states():
+
+welcome_message = "Welcome to the State Capitols game! Type the Capital associated with the State to win."
+print(welcome_message)
+
+in_game_message = "Please name the capitol of" 
+print(in_game_message)
+if state.name == capital
+print(correct)
+elif print (you are wrong)
+
+end_message ="Would you like to play again?"
+print(end_message)
+if yes == yes
+# restart game
+
+
+
+user_answer = input(in_game_message)
+
+
+
+var = states[0]["capital"]
+print(var)
